c7ccd82e3e50702c9b1e016255214f70c1da7c33
AEssabere <91161659+AEssabere@users.noreply.github.com>
2023-05-16 14:08:54 +0200

    Update streamlit_app.py
    created another header

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,5 @@ streamlit.title("My parents' healthy diet !!")
 streamlit.header("Header : ")
 streamlit.text("breakfast Menu : ")
 streamlit.text("Foood ! ")
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
